f359c623fbe01d05dff1558aaa4fb761f055eb1f
Daniel Anderson <daniela@uoregon.edu>
2020-12-22 11:28:01 -0800

    styled plot

--- scripts/exploration.R
+++ scripts/exploration.R
@@ -1,10 +1,17 @@
 library(tidyverse)
+
 ggplot(mpg, aes(displ, hwy)) +
-  geom_point(alpha = 0.3, stroke = 0, size = 2) +
+  geom_point(aes(color = drv),
+             stroke = 0, 
+             size = 2) +
   geom_smooth() +
+  scale_color_brewer("Drivetrain", palette = "Dark2") +
   theme_minimal(base_size = 15) +
-  labs(title = "The definitive ggplot2 plot") +
-  theme(plot.title.position = "plot")
+  labs(title = "The definitive ggplot2 plot",
+       x = "Engine Displacement",
+       y = "Highway Miles Per Gallon") +
+  theme(plot.title.position = "plot",
+        legend.position = "bottom")
 
 ggsave(here::here("plots", "ggplot-example.pdf"),
        dpi = 600, 
